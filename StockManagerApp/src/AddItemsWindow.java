4a8481ca563392b7923eae2efb41d6e63f4c8a12
Peter Clarke <peterclarkedublin@live.com>
2015-09-30 21:21:13 +0100

    Added gui/add products by qty added product qty calls

--- StockManagerApp/src/AddItemsWindow.java
+++ StockManagerApp/src/AddItemsWindow.java
@@ -33,7 +33,7 @@ public class AddItemsWindow extends JFrame {
 	String[][] skuArray = null;
 	private Connection skuConn;
 	String myDriver = "org.gjt.mm.mysql.Driver";
-	String myUrl = "jdbc:mysql://192.168.1.160/stockdb";
+	String myUrl = "jdbc:mysql://127.0.0.1/stockdb";
 
 	/**
 	 * Create the frame.
@@ -53,47 +53,8 @@ public class AddItemsWindow extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-
-		JButton btnAddToDbse = new JButton("Save");
-		btnAddToDbse.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				//add product to database
-				try
-				{
-
-					// create a mysql database connection
-
-					Class.forName(myDriver);
-					Connection conn = DriverManager.getConnection(myUrl, "smadmin", "admin");
-
-					// Item table mysql insert statement
-					String query = " insert into items (sku_id, location_id)"
-							+ " values (?, ?)";
-
-					// Item table mysql insert preparedstatement
-					PreparedStatement preparedStmt = (PreparedStatement) conn.prepareStatement(query);
-					//preparedStmt.setInt (1, );
-					//preparedStmt.setInt  (2 );
-
-					//execute the preparedstatement
-					preparedStmt.execute();
-
-					conn.close();
-				}
-				catch (Exception e2)
-				{
-					System.err.println("Got an exception!");
-					System.err.println(e2.getMessage());     
-				}
-
-			}
-		});
-		btnAddToDbse.setBounds(333, 84, 91, 23);
-		contentPane.add(btnAddToDbse);
-
 		JLabel lblNewLabel = new JLabel("Description");
-		lblNewLabel.setBounds(206, 15, 58, 14);
+		lblNewLabel.setBounds(186, 15, 58, 14);
 		contentPane.add(lblNewLabel);
 
 		JLabel lblNewLabel_1 = new JLabel("SKU");
@@ -120,26 +81,26 @@ public class AddItemsWindow extends JFrame {
 
 			// create a mysql database connection
 			String myDriver = "org.gjt.mm.mysql.Driver";
-			String myUrl = "jdbc:mysql://192.168.1.160/stockdb";
+			String myUrl = "jdbc:mysql://127.0.0.1/stockdb";
 			Class.forName(myDriver);
 			skuConn = DriverManager.getConnection(myUrl, "smadmin", "admin");
-			
+
 			//SKU database
-			String skuQuery = "SELECT * FROM sku";			
-		      
+			String skuQuery = "SELECT * FROM sku";
+
 			// create the java statement
 			java.sql.Statement st = skuConn.createStatement();
 
-			// execute the query, and get a java resultset		
-			
+			// execute the query, and get a java resultset
+
 			ResultSet skuRs = st.executeQuery(skuQuery);
-			
+
 			int numCounter = 0;
-			
+
 			for(numCounter = 0; skuRs.next(); numCounter++);
-			
+
 			skuArray = new String[numCounter][5];
-			
+
 			skuRs.beforeFirst();
 			numCounter = 0;
 
@@ -156,20 +117,20 @@ public class AddItemsWindow extends JFrame {
 				skuArray[numCounter][3] = upc;
 				skuArray[numCounter][4] = String.valueOf(manufacId);
 				numCounter++;
-				
+
 				//create 1D array from skuArray for combox selector
-				
+
 				skuComboArray = new String[skuArray.length];
-				
+
 				for(int i = 0; i < skuComboArray.length;i++){
-					skuComboArray[i] = skuArray[i][2];	
+					skuComboArray[i] = skuArray[i][2];
 				}
-				
+
 				for(String str: skuComboArray){
-					
+
 					System.out.println(str);
 				}
-				
+
 			}
 
 			skuConn.close();
@@ -180,22 +141,29 @@ public class AddItemsWindow extends JFrame {
 			System.err.println(e.getMessage());
 			e.printStackTrace();
 		}
-		
+
 		final JLabel manufDesc = new JLabel("...");
 		manufDesc.setBounds(348, 40, 65, 14);
 		contentPane.add(manufDesc);
-		
+
 		final JLabel descDesc = new JLabel("...");
-		descDesc.setBounds(206, 40, 72, 14);
+		descDesc.setBounds(186, 40, 92, 14);
 		contentPane.add(descDesc);
-		
-		
+
+		final JLabel idDesc = new JLabel("...");
+		idDesc.setBounds(154, 40, 46, 14);
+		contentPane.add(idDesc);
+
+
 		final JComboBox skuComboBox = new JComboBox(skuComboArray);
+		skuComboBox.setToolTipText("Please Select a Item SKU");
+		skuComboBox.setSelectedItem(null);
 		skuComboBox.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent arg0) {
-				descDesc.setText(skuArray[skuComboBox.getSelectedIndex()][1]);		
+				descDesc.setText(skuArray[skuComboBox.getSelectedIndex()][1]);
+				idDesc.setText(String.valueOf(skuArray[skuComboBox.getSelectedIndex()][0]));
 
-				// execute the query, and get a java resultset		
+				// execute the query, and get a java resultset
 				try {
 					skuConn = DriverManager.getConnection(myUrl, "smadmin", "admin");
 					ResultSet manufRs = skuConn.createStatement().
@@ -207,8 +175,7 @@ public class AddItemsWindow extends JFrame {
 				}
 			}
 		});
-		
-		
+
 		skuComboBox.setBounds(62, 34, 82, 20);
 		contentPane.add(skuComboBox);
 
@@ -217,10 +184,10 @@ public class AddItemsWindow extends JFrame {
 		contentPane.add(lblQty);
 
 		qtyTextField = new JTextField();
+		lblQty.setLabelFor(qtyTextField);
 		qtyTextField.setBounds(10, 34, 42, 20);
 		contentPane.add(qtyTextField);
 		qtyTextField.setColumns(10);
-
 		JLabel upcDesc = new JLabel("...");
 		upcDesc.setBounds(288, 40, 46, 14);
 		contentPane.add(upcDesc);
@@ -229,16 +196,13 @@ public class AddItemsWindow extends JFrame {
 		JComboBox locComboBox = new JComboBox(locId);
 		locComboBox.setBounds(62, 81, 82, 20);
 		contentPane.add(locComboBox);
-		
+
 		JLabel lblNewLabel_5 = new JLabel("ID");
 		lblNewLabel_5.setBounds(155, 15, 21, 14);
 		contentPane.add(lblNewLabel_5);
-		
 
-		
-		JLabel idDesc = new JLabel("...");
-		idDesc.setBounds(154, 40, 46, 14);
-		contentPane.add(idDesc);
+
+
 
 		JButton btnAddItem = new JButton("Add Item");
 		btnAddItem.addActionListener(new ActionListener() {
@@ -248,6 +212,52 @@ public class AddItemsWindow extends JFrame {
 		});
 
 
+		JButton btnAddToDbse = new JButton("Save");
+		btnAddToDbse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				//add product to database
+				try
+				{
+
+					// create a mysql database connection
+
+					Class.forName(myDriver);
+					Connection conn = DriverManager.getConnection(myUrl, "smadmin", "admin");
+
+					// Item table mysql insert statement
+					String query = " insert into item (sku_id, location_id)"
+							+ " values (?, ?)";
+
+					// Item table mysql insert preparedstatement
+					PreparedStatement preparedStmt = (PreparedStatement) conn.prepareStatement(query);
+					preparedStmt.setString (1, String.valueOf(skuArray[skuComboBox.getSelectedIndex()][0]));
+					preparedStmt.setInt  (2, 1 );
+
+					//execute the preparedstatement
+					for(int i = 0; i < Integer.valueOf(qtyTextField.getText());i++){
+
+						preparedStmt.execute();
+						
+						System.out.println("Item added # " + (i+1) + " added...");
+					}
+
+
+					conn.close();
+				}
+				catch (Exception e2)
+				{
+					System.err.println("Got an exception!");
+					System.err.println(e2.getMessage());
+				}
+
+			}
+		});
+		btnAddToDbse.setBounds(333, 84, 91, 23);
+		contentPane.add(btnAddToDbse);
+
+
 		setVisible(true);
 	}
+
 }
--- StockManagerApp/src/ItemsWindow.java
+++ StockManagerApp/src/ItemsWindow.java
@@ -24,6 +24,9 @@ public class ItemsWindow extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
+    int stockAmnt;
+    String sku;
+    int skuId;
 
 	/**
 	 * Create the frame.
@@ -31,7 +34,7 @@ public class ItemsWindow extends JFrame {
 	public ItemsWindow () {
 		setTitle("Items");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 485, 341);
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
@@ -44,7 +47,7 @@ public class ItemsWindow extends JFrame {
 		contentPane.setLayout(null);
 
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 11, 321, 251);
+		scrollPane.setBounds(10, 11, 348, 280);
 		contentPane.add(scrollPane);
 
 
@@ -56,60 +59,87 @@ public class ItemsWindow extends JFrame {
 		JButton btnExamine = new JButton("Examine");
 		btnExamine.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				//database connection
 				try
 				{
-		                       
+
 					// create a mysql database connection
 					String myDriver = "org.gjt.mm.mysql.Driver";
-					String myUrl = "jdbc:mysql://192.168.1.160/stockdb";
+					String myUrl = "jdbc:mysql://127.0.0.1/stockdb";
 					Class.forName(myDriver);
 					Connection conn = DriverManager.getConnection(myUrl, "smadmin", "admin");
-						
+
 				      //String query1 = "SELECT * FROM item";
-				      
-				      String query1 = "SELECT item.id, sku.sku, location.descr, manuf.descr\n" +
+
+				      String query1 = "SELECT  item.id, sku.sku, location.descr, manuf.descr, item.sku_id\n" +
 								      "from item\n" +
 								      "inner join sku\n" +
 								      "on item.sku_id=sku.id\n" +
 								      "inner join location\n" +
 								      "on item.location_id=location.id\n" +
 								      "inner join manuf\n" +
-								      "on sku.manuf_id=manuf.id";			      
+								      "on sku.manuf_id=manuf.id";
 				      
+
 				      // create the java statement
 				      java.sql.Statement st = conn.createStatement();
-				       
-				      // execute the query, and get a java resultset			
+
+
+				      // execute the query, and get a java resultset
 				      ResultSet rs = st.executeQuery(query1);
 				      
+				      
+
 				      int numCounter;
 				      for(numCounter = 0; rs.next(); numCounter++);
-					  String[][] testArray = new String[numCounter][3];
+					  String[][] itemArray = new String[numCounter][4];
 					  rs.beforeFirst();
 				      numCounter = 0;
-				      
+
 				      while (rs.next()){
 				        int id = rs.getInt(1);
-				        String sku = rs.getString(2);
+				        sku = rs.getString(2);
+				        skuId = rs.getInt(5);
 				        String loc = rs.getString(3);
+				        
 
-				         
+				                
+				        
+				        //System.out.println(queryStockCount);
+				        
 				        // print the results
 				        System.out.println("" + id + sku + loc);
+
+						itemArray[numCounter][0] = String.valueOf(id);
+						itemArray[numCounter][1] = sku;
+						itemArray[numCounter][2] = loc;
+						
+				        //count stock by SKU 
+				        String queryStockCount =  "SELECT sku_id, count(1) as count\n" + 
+				        							"from stockdb.item\n" +
+				        							"WHERE sku_id ='"+skuId+"'\n" +
+				        							"group by sku_id" ;
+				        java.sql.Statement st1 = conn.createStatement();
+				        ResultSet countS = st1.executeQuery(queryStockCount);
 				        
-						testArray[numCounter][0] = String.valueOf(id);
-						testArray[numCounter][1] = sku;
-						testArray[numCounter][2] = loc;
+				        if(countS.next()){
+				        stockAmnt = countS.getInt(2);
+				        }
+						
+						itemArray[numCounter][3] = String.valueOf(stockAmnt);
 						numCounter++;
-		                     
+						
+
+
 				      }
 
+
+				        
 						table.setModel(new DefaultTableModel(
-								testArray,new String[] {"Id" , "SKU" , "Location"}
+								itemArray,new String[] {"Id" , "SKU" , "Location" , "Qty In Stock"}
 								));
-				      
+
 
 					conn.close();
 				}
@@ -119,25 +149,25 @@ public class ItemsWindow extends JFrame {
 					System.err.println(e.getMessage());
 					e.printStackTrace();
 				}
-				
+
 
 			}
 		});
 
-		btnExamine.setBounds(341, 11, 91, 23);
+		btnExamine.setBounds(368, 8, 91, 23);
 		contentPane.add(btnExamine);
 
 		JButton btnAdjust = new JButton("Adjust");
-		btnAdjust.setBounds(341, 45, 91, 23);
+		btnAdjust.setBounds(368, 45, 91, 23);
 		contentPane.add(btnAdjust);
-		
+
 		JButton btnAddItem = new JButton("Add Item");
 		btnAddItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				new AddItemsWindow();
 			}
 		});
-		btnAddItem.setBounds(341, 79, 89, 23);
+		btnAddItem.setBounds(370, 79, 89, 23);
 		contentPane.add(btnAddItem);
 		contentPane.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{btnExamine, btnAdjust}));
 
